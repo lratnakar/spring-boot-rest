fbe9866694fb812939ce10c8be1301611ed93b06
Ratnakar Luchmapurkar <lratnakar@gmail.com>
2026-02-24 15:21:28 -0500

    initial commit

---
+++ rest-api-service/src/main/java/com/boot/rest/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.boot.rest.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam String user) {
+		return "Hello Mr. "+user;
+	}
+}
